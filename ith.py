5cc8aa8d1443efb02d80c9833415491d04023df4
snigdha manepalli <snigdha.mrm@gmail.com>
2020-05-21 22:51:22 +0530

    removing ith character of a string

---
+++ ith.py
@@ -0,0 +1,14 @@
+def ith(str):
+    # new=""
+    # for i in range(len(str)):
+    #     if i!=2:
+    #         new=new+str[i]
+    # print(new)
+    new_string=""
+    new_string=str.replace('e','',1)
+    print(new_string)
+
+
+
+str="GeeksforGeeks"
+ith(str)
